8a1bf2368823d02e5ac8c67602fa376ed3392a25
Lordify95 <lordify95@protonmail.com>
2024-12-18 14:08:24 +0000

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,27 @@
+##########################################################################################
+#
+# MMT Extended Config Script
+#
+##########################################################################################
+
+##########################################################################################
+# Config Flags
+##########################################################################################
+
+# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
+# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
+# Uncomment PARTOVER if you have a workaround in place for extra partitions in regular magisk install (can mount them yourself - you will need to do this each boot as well). If unsure, keep commented
+# Uncomment PARTITIONS and list additional partitions you will be modifying (other than system and vendor), for example: PARTITIONS="/odm /product /system_ext"
+MINAPI=28
+#MAXAPI=25
+#DYNLIB=true
+#PARTOVER=true
+#PARTITIONS=""
+
+##########################################################################################
+# MMT Extended Logic - Don't modify anything after this
+##########################################################################################
+
+SKIPUNZIP=1
+unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
+. $TMPDIR/functions.sh
